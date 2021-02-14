826ad03de7fc53bc7e85ceef9d069349c58584ae
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-14 23:28:50 +0900

    fix: ボタン位置修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,52 @@
 import React from 'react';
 import liff from '@line/liff';
-import Button from '@material-ui/core/Button';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { TertiaryButton, NomalButton } from './components/buttons'
-import Header from './components/header'
+// import Header from './components/header'
 import Home from './pages/home'
 import './App.css';
 
 function App() {
-  liff.init({liffId: process.env.REACT_APP_LIFF_ID as string}) // LIFF IDをセットする
-  .then(() => {})
+  if(liff.isInClient()){
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID as string}) // LIFF IDをセットする
+    .then(() => {})
+  }
   /* 追加: メッセージ送信 */
   const sendMessage = () => {
-    // liff.init({liffId: process.env.REACT_APP_LIFF_ID as string}) // LIFF IDをセットする
-    //   .then(() => {
-        if (!liff.isLoggedIn()) {
-          liff.login({}) // ログインしていなければ最初にログインする
-        } else if (liff.isInClient()) { // LIFFので動いているのであれば
-          liff.sendMessages([{ // メッセージを送信する
-            'type': 'text',
-            'text': "You've successfully sent a message! Hooray!"
-          }]).then(function() {
-            window.alert('Message sent');
-          }).catch(function(error) {
-            window.alert('Error sending message: ' + error);
-          });
-        }
-      // })
+    if (!liff.isLoggedIn()) {
+      liff.login({}) // ログインしていなければ最初にログインする
+    } else if (liff.isInClient()) { // LIFFので動いているのであれば
+      liff.sendMessages([{ // メッセージを送信する
+        'type': 'text',
+        'text': "You've successfully sent a message! Hooray!"
+      }]).then(function() {
+        window.alert('Message sent');
+      }).catch(function(error) {
+        window.alert('Error sending message: ' + error);
+      });
+    }
   }
 
   /* 追加: UserProfileをAlertで表示 */
   const getUserInfo = () => {
-    // liff.init({liffId: process.env.REACT_APP_LIFF_ID as string})
-    //   .then(() => {
-        if (!liff.isLoggedIn()) {
-          liff.login({}) // ログインしていなければ最初にログインする
-        } else if (liff.isInClient()) {
-          liff.getProfile()  // ユーザ情報を取得する
-            .then(profile => {
-              const userId: string = profile.userId
-              const displayName: string = profile.displayName
-              alert(`Name: ${displayName}, userId: ${userId}`)
-            }).catch(function(error) {
-              window.alert('Error sending message: ' + error);
-            });
-        }
-      // })
+    if (!liff.isLoggedIn()) {
+      liff.login({}) // ログインしていなければ最初にログインする
+    } else if (liff.isInClient()) {
+      liff.getProfile()  // ユーザ情報を取得する
+        .then(profile => {
+          const userId: string = profile.userId
+          const displayName: string = profile.displayName
+          alert(`Name: ${displayName}, userId: ${userId}`)
+        }).catch(function(error) {
+          window.alert('Error sending message: ' + error);
+        });
+    }
   }
 
   return (
     <div className="App">
       <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <div>
       <Route exact path='/' component={Home}/>
       <Route path='/send' render={ () => <TertiaryButton className="button" onClick={sendMessage}>send message</TertiaryButton> }/>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import {PrimaryButton, SecondaryButton, TertiaryButton, NomalButton} from '../components/buttons'
+import Grid from '@material-ui/core/Grid';
+import { PrimaryButton, SecondaryButton } from '../components/buttons'
 
 export default class Home extends React.Component {
   render() {
     return (
-        <div>
+        <React.Fragment>
             <h1>Home</h1>
+            <Grid container justify="center" spacing={3}>
+            <Grid item xs={12}>
             <Link to="/send" style={{ textDecoration: 'none' }}><PrimaryButton className="button">/send</PrimaryButton></Link>
+            </Grid>
+            <Grid item xs={12}>
             <Link to="/info" style={{ textDecoration: 'none' }}><SecondaryButton className="button">/info</SecondaryButton></Link>
-        </div>
+            </Grid>
+            </Grid>
+        </React.Fragment>
     );
   }
 }
